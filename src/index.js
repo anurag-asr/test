bde9a30bb76722a5df4da97e31f48c834b8232c3
anurag-asr <anurjwt@gmail.com>
2024-09-17 12:15:40 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** Internal Dependencies */
+import FilerobotImageEditor from 'components/AssemblyPoint';
+import { TABS_IDS, TOOLS_IDS } from 'utils/constants';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export { TABS_IDS as TABS, TOOLS_IDS as TOOLS };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default FilerobotImageEditor;
